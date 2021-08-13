65c44d0c760dd4d1542016a2c0ba92bc02493997
AgusAlfenrando <48276446+AgusAlfenrando@users.noreply.github.com>
2021-08-13 12:28:16 +0700

    commit merge

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Configure the Azure provider
 terraform {
   required_providers {
@@ -24,29 +23,3 @@ resource "azurerm_resource_group" "rg" {
   name     = "RG_Terraformtesting"
   location = "South East Asia"
 }
-=======
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "=2.71.0"
-    }
-  }
-}
-
-
-
-provider "azurerm" {
-  features {}
-
-
-  
-  # subscription_id = "fa27f845-9a36-4251-a00f-adaf9c8dab32"
-  # tenant_id       = "4d237fc5-b1dd-42ef-a9e2-64887cb44b21"
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "RG_Terraformtesting"
-  location = "South East Asia"
-}
->>>>>>> bf22a67539718e8eb768f3a4a2eebc7aee6fa32f
